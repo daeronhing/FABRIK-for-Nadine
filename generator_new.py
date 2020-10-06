5b13d49ae02324a9b73b871015ceb2a4f7d95c7c
daeronhing <31173368+daeronhing@users.noreply.github.com>
2020-10-06 11:57:44 +0800

    Update generator_new.py

--- generator_new.py
+++ generator_new.py
@@ -14,14 +14,14 @@ test = {
         'Body left/right'       :   128,
         'Left shoulder up/down' :   0,
         'Right shoulder up/down':   0,
-        'Left arm up/down'      :   0,
-        'Right arm up/down'     :   0,
-        'Left arm open/close'   :   0,
-        'Right arm open/close'  :   0,
-        'Left upperarm turn'    :   0,
-        'Right upperarm turn'   :   0,
-        'Left elbow'            :   0,
-        'Right elbow'           :   0,
+        'Left arm up/down'      :   128,
+        'Right arm up/down'     :   128,
+        'Left arm open/close'   :   128,
+        'Right arm open/close'  :   128,
+        'Left upperarm turn'    :   128,
+        'Right upperarm turn'   :   128,
+        'Left elbow'            :   128,
+        'Right elbow'           :   128,
         'Left forearm turn'     :   0,
         'Right forearm turn'    :   0,
         'Left wrist'            :   0,
@@ -30,8 +30,8 @@ test = {
         }
 
 joints = ['Right shoulder up/down','Right arm up/down','Right arm open/close','Right upperarm turn','Right elbow','Right forearm turn','Right wrist']
+via_points = []
 
-import numpy as np
 import os.path
 import matplotlib.pyplot as plt
 from FABRIK_optimized import out_of_range_condition
@@ -63,11 +63,30 @@ def convertor(params):
     converted_params = []
     for i in range(len(params)):
         bin = round(255*params[i]/constraints[i])
-        # bin = np.round(255*params[i]/constraints[i])
-        if bin > 255: bin = 255
-        elif bin < 0 : bin = 0
+        if bin>255: bin = 255
+        elif bin<0: bin = 0
         converted_params.append(bin)
     return converted_params
+
+def convert_to_angle(params):
+    constraints = [9, 32, 30, 90, 105, 120, 70]
+    converted_params = []
+    for i in range(len(params)):
+        deg = (params[i]/255)*constraints[i]
+        converted_params.append(deg)
+    return converted_params
+
+def move(angle, time = 3):
+    bit = convertor(angle)
+    while (len(bit)<7):
+        bit.append(0)
+    bit.append([time])
+    global via_points
+    via_points.append(bit)
+
+def hold(time):
+    global via_points
+    via_points.append(['hold', time])
 
 def mjtg(current, setpoint, frequency, move_time):
     traj = []
@@ -78,31 +97,49 @@ def mjtg(current, setpoint, frequency, move_time):
         traj.append(value)
     return traj
 
-def create_trajectory(setpoint, frequency, move_time):
+def create_trajectory(setpoints, frequency):
     trajectory_list = []
     current = ['Right shoulder up/down', 'Right arm up/down', 'Right arm open/close', 'Right upperarm turn', 'Right elbow', 'Right forearm turn', 'Right wrist']
     current = get(current)
-    for i in range(len(setpoint)):  #obj of type 'int' has no len
-    # for i in range(len(current)):
-        traj = mjtg(current[i], setpoint[i], frequency, move_time)
-        arr = np.array(traj)
-        trajectory_list.append(arr)
-    return(trajectory_list)
+
+    for via_point in setpoints:
+        temp_list = []
+        if via_point[0] == 'hold':
+            for i in range(len(current)):
+                traj = []
+                for t in range(frequency*via_point[1]):
+                    traj.append(current[i])
+                temp_list.append(traj)
+        else:
+            for i in range(len(via_point)-1):
+                traj = mjtg(current[i], via_point[i], frequency, via_point[-1][0])
+                temp_list.append(traj)
+                current[i] = temp_list[i][-1]
+
+        if not trajectory_list:
+            trajectory_list = temp_list
+        else:
+            for j in range(len(temp_list)):
+                trajectory_list[j] = trajectory_list[j] + temp_list[j]
+    global via_points
+    via_points = []
+    return (trajectory_list)
 
 def generate_line(h,m,s,f):
     time_stamp = str('{:02}:{:02}:{:02}.{:02}').format(h,m,s,f)
     line = time_stamp + ',' + dict_values()
     return line
 
-def generate(setpoint, frequency = 30, move_time = 2):
-    trajectory = create_trajectory(setpoint, frequency, move_time)
-    listfile = open("C:\\Users\\IMI-Intern-Zijing\\Desktop\\Nadine\\listfile.txt", 'w', newline = '')
+def generate(frequency = 30):
+    global via_points
+    trajectory = create_trajectory(via_points, frequency)
+    listfile = open("C:\\Users\\IMI-Intern-Soon Hing\\Desktop\\Nadine\\listfile.txt", 'w', newline = '')
     scriptpath = os.path.realpath("listfile.txt")
     listfile.write(scriptpath)
     listfile.write('\n')
     h, m, s, f = 0, 0, 0, 0
 
-    for i in range(frequency*move_time):
+    for i in range(len(trajectory[0])):
         shoulder = trajectory[0][i]
         arm_ud   = trajectory[1][i]
         arm_oc   = trajectory[2][i]
@@ -126,20 +163,9 @@ def generate(setpoint, frequency = 30, move_time = 2):
             f = 0
             s += 1
 
-    for i in range(150):
-        line = generate_line(h,m,s,f)
-        listfile.writelines(line)
-        listfile.write('\n')
+    trajectory = create_trajectory([[0,128,128,128,128,128,0,[2]]], frequency)
 
-        if (f<29):
-            f += 1
-        else:
-            f = 0
-            s += 1
-
-    trajectory = create_trajectory([0,0,0,0,0,0,0], frequency, move_time)
-
-    for i in range(frequency*move_time):
+    for i in range(len(trajectory[0])):
         shoulder = trajectory[0][i]
         arm_ud   = trajectory[1][i]
         arm_oc   = trajectory[2][i]
